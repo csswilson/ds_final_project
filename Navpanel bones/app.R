6610e9b5b394c109df17d2a7d9ef0063c584f16b
justinnwhit <justinnwhit@gmail.com>
2022-04-26 11:47:43 -0500

    NAVPANEL

---
+++ Navpanel bones/app.R
@@ -0,0 +1,35 @@
+
+
+#library("shiny.router")
+library(shiny)
+library(tidyverse)
+library(rsconnect)
+
+
+ui <- fluidPage(
+  
+  ui <- navbarPage("My Application",
+                   tabPanel("Written Summary",p("Intro"),
+                            p("Lit Reveiw"),
+                            p("Methods and Analysis"),
+                            p("Conclusdion and Results")),
+                   tabPanel("Analysis"),
+                   tabPanel("Extra")
+  ))
+
+  #YOUR SERVER HERE !!!
+
+  server <- function(input, output) {
+   output$priceplot <- renderPlot({
+    sector_prices %>%
+
+    filter(State==input$State) %>%
+      ggplot()+
+      geom_line(aes(x=sector_prices$Year,y=sector_prices$Price,color=sector_prices$Sector))+
+    scale_y_continuous(limits = input$Price)+
+      theme_minimal()
+
+  })}
+    
+   shinyApp(ui = ui, server = server)
+
--- shiny_price_line/app.R
+++ shiny_price_line/app.R
@@ -38,34 +38,5 @@
 }
 # Shiny App Price Line Graph
 
-library(shiny)
-library(tidyverse)
-library(rsconnect)
-library(lubridate)
 
-ui <- fluidPage(
-  #sliderInput(inputId = "Year",
-  #            label="Select Year",
-  #            min = 1970,
-  #            max = 2019,
-  #            value = c(1970,2019),
-  #            sep=""),
-  selectInput("State","State", choices = "State"),
-  submitButton(text="Submit"),
-  plotOutput(outputId = "priceplot")
-)
-
-server <- function(input,output) {
-  output$priceplot <- renderPlot({
-    sector_prices %>% 
-      
-    filter(State==input$State) %>% 
-      ggplot()+
-      geom_line(aes(x=sector_prices$Year,y=sector_prices$Price,color=sector_prices$Sector))+
-    scale_y_continuous(limits = input$Price)+
-      theme_minimal()
-    
-  })
-  
-  shinyApp(ui = ui, server = server)}
   

b3bf1301a2d11577cc85b2064de33945b1c13e08
justinnwhit <justinnwhit@gmail.com>
2022-04-26 19:47:29 -0500

    ooefjwfn

--- Navpanel bones/app.R
+++ Navpanel bones/app.R
@@ -1,8 +1,14 @@
 
-
 library(shiny)
-library(tidyverse)
-library(rsconnect)
+  library(tidyverse)
+  library(ggpubr)
+  library(ggplot2)
+  library(ggthemes)      # for even more plotting themes
+  library(gganimate)
+  library(ggmap) 
+  library(rsconnect)
+
+
 
 
 ui <- fluidPage(
@@ -14,26 +20,79 @@ ui <- fluidPage(
                             p("Conclusdion and Results")),
                    tabPanel("Analysis",
                             
-                            #YOUR PAGE COMPONENETS HERE
-                            
+                    
+                            sidebarLayout(
+                              sidebarPanel(style = "position:fixed;width:inherit;",
+                                           selectInput("Year", 
+                                                       "Year", 
+                                                       choices = c(2004,2005,2007,2009,2010,2012,2014,2016,2017,2018,2019)),
+                                           radioButtons("Sector", "Electricity Sector", 
+                                                        choices = c("Commercial", "Residential", "Industrial"),
+                                                        selected = "Residential"),
+                                           mainPanel(
+                                             h1("Renewable Energy and Electricity Prices in the US", align = "center"),
+                                             h3("Do higher percentages of renewables lead to a decrease in energy price in the US?",  align = "center"),
+                                             #       fluidRow(
+                                             #         splitLayout(cellWidths = c("50%", "50%"), plotOutput('renewable_map'), plotOutput('price_map'))
+                                             #       ),
+                                             plotOutput('renewable_map'), 
+                                             plotOutput('price_map'),
+                                             plotOutput("renewable_plot")
+                              )
+
                             
                             ),
                    tabPanel("Extra")
-  ))
+                   
+                   
+  ))))
 
-  #YOUR SERVER HERE !!!
+server <- function(input, output){
+  
+  
+  
+  output$renewables_map <-renderPlot(price_by_renewables %>% 
+                                      filter(Sector == input$Sector,
+                                             Year == input$Year) %>%        
+                                      ggplot() +
+                                      geom_map(map = states_map,
+                                               aes(map_id = states_map$region,
+                                                   fill = Percent)) +
+                                      expand_limits(x = states_map$long, y = states_map$lat) + 
+                                      theme_map() +
+                                      labs(title = "Percent Renewable Energy Mix per State", fill = "Percent") +
+                                      scale_fill_continuous(low = "#edf6b2" , high = "#1c5f07"),
+                                    height = 400, width = 600
+  )
+  
+  output$price_map <-renderPlot(price_by_renewables %>% 
+                                  filter(Sector == input$Sector,
+                                         Year == input$Year) %>%        
+                                  ggplot() +
+                                  geom_map(map = states_map,
+                                           aes(map_id = states_map$region,
+                                               fill = Price)) +
+                                  expand_limits(x = states_map$long, y = states_map$lat) + 
+                                  theme_map() +
+                                  labs(title = "Electricity Price per State", fill = "Price\n$ / BTU") +
+                                  scale_fill_continuous(low = "slategray1" , high = "slategray"),
+                                height = 400, width = 600
+  )
+  
+  output$renewable_plot <- renderPlot(
+    price_by_renewables %>% 
+      filter(Sector == input$Sector,
+             Year == input$Year) %>% 
+      ggplot(aes(y= Percent, x= Price))+
+      geom_point() +
+      stat_cor( aes( label = paste(..rr.label.., ..p.label.., sep = "*`,`~"))) +
+      theme_minimal() +
+      #geom_smooth(se = FALSE, method = "lm", color = "darkgreen") +
+      labs(title = "Percent Renewable Mix per Price", y = "$ / BTU", x = "%")
+  )
+  
+}
+shinyApp(ui = ui, server = server)
 
-  server <- function(input, output) {
-   output$priceplot <- renderPlot({
-    sector_prices %>%
 
-    filter(State==input$State) %>%
-      ggplot()+
-      geom_line(aes(x=sector_prices$Year,y=sector_prices$Price,color=sector_prices$Sector))+
-    scale_y_continuous(limits = input$Price)+
-      theme_minimal()
-
-  })}
-    
-   shinyApp(ui = ui, server = server)
 
